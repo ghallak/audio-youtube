98e8e421202d127f85e831c163198d5af1f2e203
Gaith Hallak <gaithhallak@gmail.com>
2016-11-28 17:15:41 +0300

    added action bar back button to AddToPlaylistActivity

--- app/src/main/java/com/ss/proj/activities/AddToPlaylistActivity.java
+++ app/src/main/java/com/ss/proj/activities/AddToPlaylistActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.ss.proj.R;
 import com.ss.proj.fragments.PlaylistsFragment;
@@ -18,5 +19,17 @@ public class AddToPlaylistActivity extends Activity {
 		FragmentTransaction ft = getFragmentManager().beginTransaction();
 		ft.replace(R.id.add_to_playlist_child_fragment, fragment);
 		ft.commit();
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				finish();
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
