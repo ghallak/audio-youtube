41608149e7ce6102dd3bebe4163b748461c6ff13
Gaith Hallak <gaithhallak@gmail.com>
2016-11-29 19:20:49 +0300

    fixed database schema

--- app/src/main/java/com/ss/proj/database/AudioPlayerHelper.java
+++ app/src/main/java/com/ss/proj/database/AudioPlayerHelper.java
@@ -4,28 +4,32 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.ss.proj.database.AudioPlayerContract.AudioTrackEntry;
+import com.ss.proj.database.AudioPlayerContract.PlaylistAudioTrackEntry;
+import com.ss.proj.database.AudioPlayerContract.PlaylistEntry;
+
 public class AudioPlayerHelper extends SQLiteOpenHelper {
 
 	public static final int DATABASE_VERSION = 1;
 	public static final String DATABASE_NAME = "AudioPlayer.db";
 
-	private static final String TEXT_TYPE = "TEXT";
-	private static final String INTEGER_TYPE = "INTEGER";
+	private static final String TEXT_TYPE = " TEXT";
+	private static final String INTEGER_TYPE = " INTEGER";
 	private static final String COMMA_SEP = ",";
 	private static final String SQL_CREATE_AUDIO_TRACKS_TABLE =
-			"CREATE TABLE " + AudioPlayerContract.AudioTrackEntry.TABLE_NAME + " (" +
-					AudioPlayerContract.AudioTrackEntry._ID + " INTEGER PRIMARY KEY," +
-					AudioPlayerContract.AudioTrackEntry.COLUMN_NAME_VIDEO_ID + TEXT_TYPE + COMMA_SEP +
-					AudioPlayerContract.AudioTrackEntry.COLUMN_NAME_TITLE + TEXT_TYPE + " )";
+			"CREATE TABLE " + AudioTrackEntry.TABLE_NAME + " (" +
+					AudioTrackEntry._ID + " INTEGER PRIMARY KEY," +
+					AudioTrackEntry.COLUMN_NAME_VIDEO_ID + TEXT_TYPE + COMMA_SEP +
+					AudioTrackEntry.COLUMN_NAME_TITLE + TEXT_TYPE + " )";
 	private static final String SQL_CREATE_PLAYLISTS_TABLE =
-			"CREATE TABLE" + AudioPlayerContract.PlaylistEntry.TABLE_NAME + " (" +
-					AudioPlayerContract.PlaylistEntry._ID + "INTEGER PRIMARY KEY," +
-					AudioPlayerContract.PlaylistEntry.COLUMN_NAME_TITLE + TEXT_TYPE + " )";
+			"CREATE TABLE " + PlaylistEntry.TABLE_NAME + " (" +
+					PlaylistEntry._ID + " INTEGER PRIMARY KEY," +
+					PlaylistEntry.COLUMN_NAME_TITLE + TEXT_TYPE + " )";
 	private static final String CREATE_PLAYLIST_AUDIO_TRACK_TABLE =
-			"CREATE TABLE" + AudioPlayerContract.PlaylistAudioTrackEntry.TABLE_NAME + " (" +
-					AudioPlayerContract.PlaylistAudioTrackEntry._ID + "INTEGER PRIMARY KEY," +
-					AudioPlayerContract.PlaylistAudioTrackEntry.COLUMN_NAME_AUDIO_ID + INTEGER_TYPE + COMMA_SEP +
-					AudioPlayerContract.PlaylistAudioTrackEntry.COLUMN_NAME_PLAYLIST_ID + INTEGER_TYPE + " )";
+			"CREATE TABLE " + PlaylistAudioTrackEntry.TABLE_NAME + " (" +
+					PlaylistAudioTrackEntry._ID + " INTEGER PRIMARY KEY," +
+					PlaylistAudioTrackEntry.COLUMN_NAME_AUDIO_ID + INTEGER_TYPE + COMMA_SEP +
+					PlaylistAudioTrackEntry.COLUMN_NAME_PLAYLIST_ID + INTEGER_TYPE + " )";
 
 	public AudioPlayerHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -40,6 +44,5 @@ public class AudioPlayerHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
 	}
 }
